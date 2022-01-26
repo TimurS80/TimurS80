70192069949bb0db239f17cacc2bd24f782c8288
TimurS80 <olesiavld@mail.ru>
2022-01-26 12:48:14 +1000

    !2220

--- src/java1/Task6.java
+++ src/java1/Task6.java
@@ -7,7 +7,7 @@ public class Task6 {
         System.out.println("Введите значение  числа и его степени");
         Scanner scanner = new Scanner(System.in);
         //инициализация переменной из ввода
-        int a = scanner.nextInt();// вывод и сохр мл разряда
+        int a = scanner.nextInt();// вывод и сохр мл разр
         int b = scanner.nextInt();// вывод и сохр мл разряда
         long y;
         y = (long) Math.pow(a,b);
